b65c35e9d124493d4957e5e593e9d8d94fa9ff47
Projet LP <ravet.antoine.eln@gmail.com>
2017-01-23 17:58:55 +0100

    Avancement dans les exercices

---
+++ src/app/models/race.model.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by user on 23/01/2017.
+ */
+
+export interface RaceModel{
+    name: string;
+}
---
+++ src/app/races/races.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { RacesComponent } from './races.component';
+
+describe('Component: Races', () => {
+  it('should create an instance', () => {
+    let component = new RacesComponent();
+    expect(component).toBeTruthy();
+  });
+});
